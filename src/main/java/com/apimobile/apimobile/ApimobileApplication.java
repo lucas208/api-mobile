ea3b9b7282ddbe3b438ee498cef7b1bfa0d8bbce
Lucas Vieira <lucas.vieira.093@ufrn.edu.br>
2022-09-19 20:16:11 -0300

    Commit inicial

---
+++ src/main/java/com/apimobile/apimobile/ApimobileApplication.java
@@ -0,0 +1,13 @@
+package com.apimobile.apimobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimobileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apimobile/apimobile/ApimobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apimobile.apimobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
